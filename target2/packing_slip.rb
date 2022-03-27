304808d7ddbbee91624990e9fb302e8fc5e722bb
sh1okoh <kooooo.0719task@gmail.com>
2022-03-27 14:31:34 +0900

    add

--- target2/packing_slip.rb
+++ target2/packing_slip.rb
@@ -1,4 +1,16 @@
 # NOTE: 積荷票
 class PackingSlip
-  def initialize; end
+  def initialize(container, time_zone_now)
+    # NOTE:
+    # - コンテナ番号
+    # - 搬入年月日、日時
+    # - 内蔵品名, 数量
+    @container_number = container.number
+    @delivery_date = time_zone_now
+    @alcohol_info = make_alcohol_info(container)
+  end
+
+  def make_alcohol_info(container)
+    alcohol_info = Array.new(Hash.new)
+  end
 end
