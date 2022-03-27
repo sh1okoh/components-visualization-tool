f6d66d2e49a9b050721b0bbdb27b6f2ea1dce5e0
sh1okoh <kooooo.0719task@gmail.com>
2022-03-27 15:35:45 +0900

    add

--- target2/packing_slip.rb
+++ target2/packing_slip.rb
@@ -5,15 +5,17 @@ class PackingSlip
     # - コンテナ番号
     # - 搬入年月日、日時
     # - 内蔵品名, 数量
-    #   - [ { name: "内蔵品名", num: 数量 } ]
+    #   - [ { "内蔵品名": 数量 } ]
     @container_number = container.number
     @delivery_date = time_zone_now
     @alcohol_info = make_alcohol_info(container)
   end
 
+  private
+
   def make_alcohol_info(container)
     alcohol_store = container.alcohol_store
-    info = alcohol_store.group_by(&:itself).map { |key, value| [key, value.count] }
+    info = alcohol_store.group_by(&:itself).map { |key, value| [key, value.count] }.to_h
     info
   end
 end
