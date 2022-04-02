823d0fa570ab85354e1d067d971e0a301d3f9b3b
sh1okoh <kooooo.0719task@gmail.com>
2022-04-02 23:24:17 +0900

    add

--- target2/warehouse_service.rb
+++ target2/warehouse_service.rb
@@ -1,12 +1,21 @@
 class WarehouseService
   def run
     # 倉庫係がコンテナを受け取る
+    container = make_container
     # 倉庫係がコンテナを倉庫に保管する
+    WarehouseWorker.store(container)
+    packing_slip = PackingSlip.new(container, Time.zone.now)
     # 倉庫係が積荷票を受付係へ手渡す
+    Receptionist.new.receive_packing_slip(packing_slip)
     # 受付係の出庫指示によって内蔵品を出庫する
     # - 受付係は毎日数10件の出庫依頼を受け、倉庫係へ出庫指示書を出す
     # - 出庫依頼は出庫依頼票もしくは電話、1件1銘柄
     # - 在庫がないか、不足の場合にはその旨を依頼者に電話連絡し、同時に在庫不足リストに記入する
     # - また空になる予定のコンテナを倉庫係に知らせることになっている
+  end
+
+  def make_container
+    alcohol = Alcohol.new('a')
+    Container.new.append(alcohol)
   end
 end
