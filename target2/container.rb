010e7284a438d0115fca68e2ce7ee95430c02574
sh1okoh <kooooo.0719task@gmail.com>
2022-04-03 21:16:00 +0900

    add

--- target2/container.rb
+++ target2/container.rb
@@ -3,6 +3,10 @@ class Container
 
   def initialize
     @alcohol_store = Array.new
+    # TODO: コンテナ番号を計算して出すようにする
+    # - 実装案:
+    #   - container を保村しているテーブルから、コンテナ数を数えて、コンテナ数+1をした番号にする
+    #   - 適当にユニークな番号にする
     @number = 1
   end
 
