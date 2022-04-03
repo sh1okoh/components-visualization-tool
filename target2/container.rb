87b2a2459314d583fa9f0d423e27f5aaec8e4032
sh1okoh <kooooo.0719task@gmail.com>
2022-04-03 21:20:37 +0900

    add

--- target2/container.rb
+++ target2/container.rb
@@ -2,15 +2,15 @@ class Container
   attr_accessor :alcohol_store, :number
 
   def initialize
-    @alcohol_store = Array.new
+    @alcohol_table = Table.new
     # TODO: コンテナ番号を計算して出すようにする
     # - 実装案:
-    #   - container を保村しているテーブルから、コンテナ数を数えて、コンテナ数+1をした番号にする
+    #   - container を保存しているテーブルから、コンテナ数を数えて、コンテナ数+1をした番号にする
     #   - 適当にユニークな番号にする
     @number = 1
   end
 
   def append(alcohol)
-    @alcohol_store.append(alcohol)
+    @alcohol_table.rep.append(alcohol)
   end
 end
